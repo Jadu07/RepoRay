8739843b51b369c57040266394c1e2e000d1da18
Yashraj Chouhan <yashrajchouhan14@gmail.com>
2025-09-28 00:15:00 +0530

    added backend api link

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const SERVER_URL = 'http://localhost:3001/api';
+const SERVER_URL = 'https://repopay.onrender.com/api';
 export const getRepositoryAnalysis = async (owner, repo) => {
   const res = await fetch(`${SERVER_URL}/summary/${owner}/${repo}`);
   if (!res.ok) {
